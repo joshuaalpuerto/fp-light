4f060be585c0216dff5e86145068c3fe3f6f172f
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-10-05 22:24:16 +0300

    Add ifElse utility

---
+++ src/ifElse.js
@@ -0,0 +1,13 @@
+/**
+ * Returns function that if predicate is evaluate correctly 
+ * will evaluate the true statement else evaluate the false.
+ * TODO: work on signature, make this curry function
+ * @param {*} predicate 
+ * @param {*} truthy 
+ * @param {*} falsy 
+ */
+const ifElse = (predicate, truthy, falsy) => (...args) =>
+  predicate(...args) ? truthy(...args) : falsy(...args)
+
+
+export default ifElse
