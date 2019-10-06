bfcdd9cfac9542384faa74370fa0a95e8c692c6a
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-10-06 14:50:22 +0300

    Add negation

---
+++ src/not.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the negate result of calling argument
+ * TODO: work on signature, make this curry function
+ * @param {*} predicate 
+ * @param {*} trueFn 
+ */
+const not = (arg) =>
+  !arg
+
+
+export default not
