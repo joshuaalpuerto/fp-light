2ab04d2cfb7c791974999bab37d0a42ff18eeded
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-10-27 16:36:52 +0200

    Add identity function

---
+++ src/identity.js
@@ -0,0 +1,9 @@
+/**
+ * A function that returns the same value
+ * Why since identity function acts like a neutral value in functions 
+ * Just like 0 to number. where it doesn't return value
+ * This is very useful in HOF wehere we just ned the value
+ * @param {*} val 
+ */
+const identity = (val) => val
+export default identity
