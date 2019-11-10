c768d8ae116a39c8c834c12b9e2d6ffac082dcad
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-11-10 15:58:57 +0200

    Expose entry point

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as complement } from './complement'
+export { default as identity } from './identity'
+export { default as ifElse } from './ifElse'
+export { default as not } from './not'
+export { default as when } from './when'
